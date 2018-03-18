04cac4f5ee8753cfc67456898e9d2306444b0396
王碩志 <wangsc@wangshuozhide-MacBook-Pro.local>
2018-03-19 01:14:02 +0800

    add_os_isfir

--- products.py
+++ products.py
@@ -1,14 +1,22 @@
+#
+import os # operating system
+
 #讀取檔案
 products = []
-with open('products.csv', 'r', encodig='utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue
-		name, price = line.strip().split(',')
-		#name = s[0]
-		#price = s[1]
-		products.append([name, price])
-print(products)
+if os.path.isfile('products.csv'): # 檢查檔案在不在
+	print('yeah! 找到檔案了!')
+	with open('products.csv', 'r', encodig='utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue
+			name, price = line.strip().split(',')
+			#name = s[0]
+			#price = s[1]
+			products.append([name, price])
+	print(products)
+
+else:
+	print('找不到檔案......')
 
 #使用者輸入
 while True:
