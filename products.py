1e8f7d3542aab1287e55751e9da887860325473a
王碩志 <wangsc@wangshuozhide-MacBook-Pro.local>
2018-03-18 20:40:52 +0800

    ver.5_end

--- products.py
+++ products.py
@@ -1,18 +1,16 @@
 #讀取檔案
+products = []
 with open('products.csv', 'r', encodig='utf-8') as f:
 	for line in f:
+		if '商品,價格' in line:
+			continue
 		name, price = line.strip().split(',')
 		#name = s[0]
 		#price = s[1]
 		products.append([name, price])
 print(products)
 
-
-
-
-
-
-products = []
+#使用者輸入
 while True:
 	name = input('請輸入商品名稱：')
 	if name == 'q':
@@ -23,10 +21,11 @@ while True:
 	#p.append(price)
 	products.append([name, price])
 #print(products)
-
+#印出所有購買紀錄
 for p in products:
 	print(p[0], '的價格是', p[1])
 
+#寫入檔案
 with open('products.csv', 'w', encoding='utf-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
